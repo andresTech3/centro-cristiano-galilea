9903936a5c5eb8e3e8ca3b8e3ccbfbe930a944bf
andres <andresnarvaez304@gmail.com>
2021-01-07 08:54:18 -0500

    mejorando rendimiento de codigo

--- JS/animation.js
+++ JS/animation.js
@@ -1,71 +1,33 @@
-//index | jovenes
+function anim(nombre, animate) {
+    let grup_iglesia;
+    window.addEventListener("scroll", () => {
+        grup_iglesia = document.querySelector(nombre);
+        let position_grup_iglesia = grup_iglesia.getBoundingClientRect().top;
+        let tamPan_grupIglesia = window.innerHeight / 1;
+        if (position_grup_iglesia < tamPan_grupIglesia) {
+            grup_iglesia.style.animation = animate;
+        }
+    })
+}
 
-window.addEventListener("scroll", () => {
-    let grup_iglesia = document.querySelector(".anim-jovenes");
-    let position_grup_iglesia = grup_iglesia.getBoundingClientRect().top;
-    let tamPan_grupIglesia = window.innerHeight / 1;
-    if (position_grup_iglesia < tamPan_grupIglesia) {
-        grup_iglesia.style.animation = "anim-grup-iglesia 3s";
-    }
-})
+//index | jovenes
+anim(".anim-jovenes", "anim-grup-iglesia 3s");
 
 // index | damas
-window.addEventListener("scroll", () => {
-    let grup_iglesia = document.querySelector(".anim-damas");
-    let position_grup_iglesia = grup_iglesia.getBoundingClientRect().top;
-    let tamPan_grupIglesia = window.innerHeight / 1;
-    if (position_grup_iglesia < tamPan_grupIglesia) {
-        grup_iglesia.style.animation = "anim-grup-iglesia 3s";
-    }
-})
+anim(".anim-damas", "anim-grup-iglesia 3s");
 
 // index / caballero
-window.addEventListener("scroll", () => {
-    let grup_iglesia = document.querySelector(".anim-caballero");
-    let position_grup_iglesia = grup_iglesia.getBoundingClientRect().top;
-    let tamPan_grupIglesia = window.innerHeight / 1;
-    if (position_grup_iglesia < tamPan_grupIglesia) {
-        grup_iglesia.style.animation = "anim-grup-iglesia 3s";
-    }
-})
+anim(".anim-caballero", "anim-grup-iglesia 3s");
+
 
 // musica | adoracion
-window.addEventListener("scroll", () => {
-    let grup_iglesia = document.querySelector(".anim-adoracion");
-    let position_grup_iglesia = grup_iglesia.getBoundingClientRect().top;
-    let tamPan_grupIglesia = window.innerHeight / 1;
-    if (position_grup_iglesia < tamPan_grupIglesia) {
-        grup_iglesia.style.animation = "move 2s";
-    }
-})
+anim(".anim-adoracion", "move 2s");
 
 // musica |alabanza
-window.addEventListener("scroll", () => {
-    let grup_iglesia = document.querySelector(".anim-alabanza");
-    let position_grup_iglesia = grup_iglesia.getBoundingClientRect().top;
-    let tamPan_grupIglesia = window.innerHeight / 1;
-    if (position_grup_iglesia < tamPan_grupIglesia) {
-        grup_iglesia.style.animation = "move1 2s";
-    }
-})
+anim(".anim-alabanza", "move 2s");
 
+//clases | clase-musical
+anim(".anim-move-musica", "move1 2s");
 
-//clase | anim-move-musica
-window.addEventListener("scroll", () => {
-    let grup_iglesia = document.querySelector(".anim-move-musica");
-    let position_grup_iglesia = grup_iglesia.getBoundingClientRect().top;
-    let tamPan_grupIglesia = window.innerHeight / 1;
-    if (position_grup_iglesia < tamPan_grupIglesia) {
-        grup_iglesia.style.animation = "move1 2s";
-    }
-})
-
-// clase | move-anim
-window.addEventListener("scroll", () => {
-    let grup_iglesia = document.querySelector(".move-anim");
-    let position_grup_iglesia = grup_iglesia.getBoundingClientRect().top;
-    let tamPan_grupIglesia = window.innerHeight / 1;
-    if (position_grup_iglesia < tamPan_grupIglesia) {
-        grup_iglesia.style.animation = "move 2s";
-    }
-})
+// clases | clase-cdi
+anim(".move-anim", "move 2s")
